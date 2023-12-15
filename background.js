42bcc2fd83853252aa6b1585033cb1ad3ea2766f
Gilead <nashar101@gmail.com>
2023-12-15 09:38:37 +0000

    fixed content script file not working on website load

--- background.js
+++ background.js
@@ -1,5 +1,41 @@
+const generateHTML = pageName => {
+    return (
+        '<!DOCTYPE html>\n' +
+        '<html lang="en">\n' +
+        '<head>\n' +
+        '<meta charset="UTF-8">\n' +
+        '<title>Blocked</title>\n' +
+        '<link rel="stylesheet" type="text/css" href="style.css">\n' +
+        '</head>\n' +
+        '<body style="align-items: center; justify-content: center; width: 100%;">\n' +
+        '<div style="border-bottom: 7px solid black;">\n' +
+        '</div>\n' +
+        '<div id="runner">\n' +
+        '</div>\n' +
+        '<div id="road">\n' +
+        '</div>\n' +
+        '<div style="border-bottom: 7px solid black; height: 200px">\n' +
+        '</div>\n' +
+        '<div style="width: 100%; height:0px;">\n' +
+        '</div>\n' +
+        '<div style="animation: myAnim 2s ease 0s 1 normal forwards; text-align: center; position: center">Sorry this website has been blocked</div>\n' +
+        '<div>\n' +
+        '<script type="text/javascript" src="Content.js">' +
+        '</script>\n' +
+        '<div id="site" style="text-align: center; animation: myAnim 2s ease 0s 1 normal forwards;">\n' +
+        '</div>\n' +
+        '</div>\n' +
+        '<div class="cloud">\n' +
+        '</div>\n' +
+        '</body>\n' +
+        '</html>'
+    );
+};
+
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
     if(request.message){
-        console.log(request.message)
+        console.log(request.message[0])
+        console.log(new Date(parseInt(request.message[1])))
+        chrome.runtime.sendMessage({action: "open_dialog_box"}, function(response) {});
     }
 });
--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const generateHTML = pageName => {
+    return (
+        '<!DOCTYPE html>\n' +
+        '<html lang="en">\n' +
+        '<head>\n' +
+        '<meta charset="UTF-8">\n' +
+        '<title>Blocked</title>\n' +
+        '<link rel="stylesheet" type="text/css" href="style.css">\n' +
+        '</head>\n' +
+        '<body style="align-items: center; justify-content: center; width: 100%;">\n' +
+        '<div style="border-bottom: 7px solid black;">\n' +
+        '</div>\n' +
+        '<div id="runner">\n' +
+        '</div>\n' +
+        '<div id="road">\n' +
+        '</div>\n' +
+        '<div style="border-bottom: 7px solid black; height: 200px">\n' +
+        '</div>\n' +
+        '<div style="width: 100%; height:0px;">\n' +
+        '</div>\n' +
+        '<div style="animation: myAnim 2s ease 0s 1 normal forwards; text-align: center; position: center">Sorry this website has been blocked</div>\n' +
+        '<div>\n' +
+        '<script type="text/javascript" src="Content.js">' +
+        '</script>\n' +
+        '<div id="site" style="text-align: center; animation: myAnim 2s ease 0s 1 normal forwards;">\n' +
+        '</div>\n' +
+        '</div>\n' +
+        '<div class="cloud">\n' +
+        '</div>\n' +
+        '</body>\n' +
+        '</html>'
+    );
+};
+
 async function fetchData(){
 
 }
@@ -5,6 +39,9 @@ async function fetchData(){
 function sayHI(){
     console.log("hi")
 }
-document.querySelector("#hi").addEventListener("click", () => {
+/*document.querySelector("#hi").addEventListener("click", () => {
     console.log("hi")
-});
+});*/
+
+alert("this is a test")
+document.body.innerHTML = generateHTML('site is blocked')
